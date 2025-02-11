385f501fd5c3f3faf8702aa9dd46cdd149426e7e
Anthony Attwood <tonesandtones@users.noreply.github.com>
2025-02-11 14:32:27 +0800

    Reworked the demo to have put the <video> offscreen and only render the canvas

--- js/main.js
+++ js/main.js
@@ -7,19 +7,69 @@
 */
 'use strict';
 
-const leftVideo = document.getElementById('leftVideo');
-const rightVideo = document.getElementById('rightVideo');
+// const leftVideo = document.getElementById('leftVideo');
+const video = createVideoDomNode();
+const videoCanvas = document.getElementById('videoCanvas');
+const canvasCtx = videoCanvas.getContext('2d');
 
-leftVideo.addEventListener('canplay', () => {
-    let stream;
-    const fps = 0;
-    if (leftVideo.captureStream) {
-        stream = leftVideo.captureStream(fps);
-    } else if (leftVideo.mozCaptureStream) {
-        stream = leftVideo.mozCaptureStream(fps);
+const playPause = document.getElementById('play-pause');
+playPause.addEventListener("click", (e) => {
+    if (video.paused || video.ended) {
+        video.play();
     } else {
-        console.error('Stream capture is not supported');
-        stream = null;
+        video.pause();
     }
-    rightVideo.srcObject = stream;
 });
+
+video.addEventListener('loadeddata', () =>{
+    videoCanvas.width = video.videoWidth;
+    videoCanvas.height = video.videoHeight;
+});
+
+video.addEventListener('canplay', () => {
+    update();
+});
+
+function update(){
+    // console.log('callback goes brrrrr');
+    let textX = 50;
+    let textY = 50;
+    let textMaxWidth = 230;
+    let globalAlpha = 1.0;
+    let textBackgroundGlobalAlpha = 0.5;
+    let fontStyle = "15px sans-serif"
+
+    let dw = videoCanvas.width;
+    let dh = videoCanvas.height;
+    canvasCtx.drawImage(video, 0, 0, dw, dh);
+
+    canvasCtx.globalAlpha = textBackgroundGlobalAlpha;
+    canvasCtx.fillStyle = "black";
+    canvasCtx.fillRect(textX - 5, textY - 15, textMaxWidth, 20);
+    
+    canvasCtx.font = fontStyle;
+    canvasCtx.fillStyle = "yellow";
+    canvasCtx.globalAlpha = globalAlpha;
+    canvasCtx.fillText(new Date().toUTCString(), textX, textY, textMaxWidth);
+    requestAnimationFrame(update);
+}
+
+function createVideoDomNode() {
+    const leftVideo = document.createElement('video');
+    const videoSrc = document.createElement('source');
+    
+    const sourceAttribute = document.createAttribute('src');
+    sourceAttribute.value = 'video/chrome.webm';
+    videoSrc.setAttributeNode(sourceAttribute);
+    
+    const typeAttribute = document.createAttribute('type');
+    typeAttribute.value = 'video/webm';
+    videoSrc.setAttributeNode(typeAttribute);
+    
+    leftVideo.setAttributeNode(document.createAttribute('loop'));
+    leftVideo.setAttributeNode(document.createAttribute('muted'));
+    
+    leftVideo.appendChild(videoSrc);
+    
+    return leftVideo;
+}
